33ac37139339ddc418f1af0f555f440f576ad7e9
Samuel L <39858145+amadeusine@users.noreply.github.com>
2019-04-23 08:53:14 -0500

    Annotated return types

--- har.py
+++ har.py
@@ -11,43 +11,43 @@ wbarray = [
 
 config = {
     'char_limit': 250,
-    'har': 'har'
+    'har': 'reee'
 }
 
-def open_links():
+def open_links() -> None:
     wb.open_new('https://www.minecraft.net/download')
     for link in wbarray:
        wb.open_new_tab(link)
 
-def fork(filename, num_children=2):
+def fork(filename, num_children=2) -> tuple:
     num = ''.join(char for char in filename[::-1] if char in '1234567890')
 
     num1 = str(int(num) * 2) if len(num) else num
     num2 = str(int(num) * 2 + 1) if len(num) else num
 
-    fnames = [
+    fnames = (
         'har' + (num1 if num1 else '1'),
         'har' + (num2 if num2 else '2')
-    ]
+    )
     return fnames
 
-def everything():
+def everything() -> None:
     print("Hello, Dave.")
     open_links()
-    
-    filename, ext = path.splitext(path.basename(__file__))
+
     directory = path.dirname(path.abspath(__file__))
+    filename, ext = path.splitext(path.basename(__file__))
+    
     
     children = fork(filename)
 
-    overflow = sum(len(child) > config['char_limit'] for child in children)
+    overflow = (child for child in children if len(child) > config['char_limit'])
 
     if overflow:
         print('resetting')
-        fnames = [f'{config['har']}{i}' for i in len(fnames)]
+        fnames = (f'{config['har']}{i}' for i in len(fnames))
 
-    fin = path.join(directory, filename+ext)
-    with open(fin) as reader:
+    with open(__file__) as reader:
         for child in children:
             fout = path.join(directory, child+ext)
             with open(fout, 'w+') as writer:
