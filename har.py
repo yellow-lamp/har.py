e3d899f84f2e58182819d25b6da05e78b8348129
Samuel Lim <39858145+amadeusine@users.noreply.github.com>
2019-08-15 12:46:19 -0500

    Update har.py

--- har.py
+++ har.py
@@ -27,7 +27,7 @@ def fork(filename, num_children = None) -> tuple:
     )
     return fnames
 
-def everything(char_limit = 250) -> None:
+def everything(char_limit = 50) -> None:
     print('Hello, Dave.')
     open_links()
 
